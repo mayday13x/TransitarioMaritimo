ff105b0cf28875e7ea59b6a60dc22ca8a4203a81
Joel Filipe Martins Amaral <131992612+Kng1011@users.noreply.github.com>
2024-04-22 15:23:21 +0100

    interface reserva

--- src/main/java/com/example/transitariomaritimo/ClienteController.java
+++ src/main/java/com/example/transitariomaritimo/ClienteController.java
@@ -2,9 +2,31 @@ package com.example.transitariomaritimo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ClienteController {
 
 
+    @FXML
+    public void VoltarAtras(ActionEvent event) {
+
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+            Scene regCena = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(regCena);
+            stage.setTitle("com.example.transitariomaritimo.Cliente");
+            stage.show();
+        }catch (IOException ex){
+            System.out.println("Erro ao acessar menu cliente: " + ex.getMessage());
+        }
+    }
 }
