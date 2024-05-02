06f220b1ff8a87848895d59e0aa3b33a088277f1
Joel Filipe Martins Amaral <131992612+Kng1011@users.noreply.github.com>
2024-05-02 21:26:46 +0100

    update Registar Reservas

--- TransitarioMaritimoDesktop/src/main/java/com/example/transitariomaritimo/ReservasController.java
+++ TransitarioMaritimoDesktop/src/main/java/com/example/transitariomaritimo/ReservasController.java
@@ -210,19 +210,8 @@ public class ReservasController implements Initializable {
             IdEstadoReservaCombo.getSelectionModel().clearSelection();
         }
 
-
-
-
-        try{
-            Parent root = FXMLLoader.load(getClass().getResource("Reservas.fxml"));
-            Scene regCena = new Scene(root);
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(regCena);
-            stage.setTitle("Reservas");
-            stage.show();
-        }catch (IOException ex){
-            System.out.println("Erro ao acessar menu cliente: " + ex.getMessage());
-        }
+        ObservableList<ReservaEntity> ReservasAtualizadas = FXCollections.observableArrayList(reserva_repo.findAll());
+        Table.setItems(ReservasAtualizadas);
     }
 
 
