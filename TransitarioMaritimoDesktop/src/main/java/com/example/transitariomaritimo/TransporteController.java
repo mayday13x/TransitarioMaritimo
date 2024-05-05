574374b4010d4eab3f69613877ceea71f61ef7cb
Joel Filipe Martins Amaral <131992612+Kng1011@users.noreply.github.com>
2024-05-05 19:35:53 +0100

    Trasnporte está a funcionar

--- TransitarioMaritimoDesktop/src/main/java/com/example/transitariomaritimo/TransporteController.java
+++ TransitarioMaritimoDesktop/src/main/java/com/example/transitariomaritimo/TransporteController.java
@@ -1,4 +1,4 @@
-/*package com.example.transitariomaritimo;
+package com.example.transitariomaritimo;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -20,94 +20,44 @@ public class TransporteController implements Initializable {
 
     public AnnotationConfigApplicationContext context;
     public TransportemaritimoRepository transportemaritim_repo;
-    public TransporteterrestreRepository transporteterrestre_repo;
+
 
     @FXML
-    private TableColumn<TransportemaritimoEntity, String> DestinoTm;
+    private TableColumn<TransportemaritimoEntity, String> Destino;
+
+
 
     @FXML
-    private TableColumn<TransporteterrestreEntity, String> DestinoTr;
-
-    @FXML
-    private TableColumn<TransporteterrestreEntity, String> IdTr;
-
-    @FXML
-    private TableColumn<TransportemaritimoEntity, String> IdTm;
+    private TableColumn<TransportemaritimoEntity, String> Id;
 
     @FXML
     private TableColumn<TransportemaritimoEntity, String> Imo;
 
-    @FXML
-    private TableColumn<TransporteterrestreEntity, String> Matricula;
+
 
     @FXML
-    private TableColumn<TransportemaritimoEntity, String> OrigemTm;
+    private TableColumn<TransportemaritimoEntity, String> Origem;
+
+
 
     @FXML
-    private TableColumn<TransporteterrestreEntity, String> OrigemTr;
+    private TableView<TransportemaritimoEntity> Table;
 
-    @FXML
-    private TableView<TransportemaritimoEntity> TableTm;
 
-    @FXML
-    private TableView<TransporteterrestreEntity> TableTr;
-
-    @FXML
-    private Pane SecondPane;
-
-    @FXML
-    private ComboBox<String> TipoTranporteCombo;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        TipoTranporteCombo.getItems().addAll("Transporte Maritimo", "TransporteTerrestre");
+
 
         context = new AnnotationConfigApplicationContext(AppConfig.class);
         transportemaritim_repo = context.getBean(TransportemaritimoRepository.class);
 
         ObservableList<TransportemaritimoEntity> transportemaritimo = FXCollections.observableArrayList(transportemaritim_repo.findAll());
 
-        IdTm.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getId().toString()));
+        Id.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getId().toString()));
         Imo.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getImo()));
-        OrigemTm.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPortoOrigem()));
-        DestinoTm.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPortoDestino()));
-
-
-
-        transporteterrestre_repo = context.getBean(TransporteterrestreRepository.class);
-        ObservableList<TransporteterrestreEntity> transporteterrestre = FXCollections.observableArrayList(transporteterrestre_repo.findAll());
-
-        IdTr.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getId().toString()));
-        Matricula.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getId().toString()));
-        OrigemTr.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getOrigem()));
-        DestinoTr.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getDestino()));
-
-
-        TipoTranporteCombo.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-
-                if( oldValue== null && newValue.equals("Transporte Maritimo")){
-                    TableTm.setItems(transportemaritimo);
-                    TableTr.setItems(transporteterrestre);
-                    SecondPane.setVisible(true);
-                    TableTr.setVisible(false);
-                }else if(oldValue == null && newValue.equals("TransporteTerrestre")){
-                    TableTr.setItems(transporteterrestre);
-                    TableTm.setItems(transportemaritimo);
-                    SecondPane.setVisible(false);
-                    TableTr.setVisible(true);
-                }
-
-
-                if (newValue.equals("Transporte Maritimo")) {
-                    SecondPane.setVisible(true);
-                    TableTr.setVisible(false);
-                } else if (newValue.equals("TransporteTerrestre")) {
-                    SecondPane.setVisible(false);
-                    TableTr.setVisible(true);
-                }
-            }
-        });
-
+        Origem.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPortoOrigem()));
+        Destino.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPortoDestino()));
+        Table.setItems(transportemaritimo);
     }
-}*/
+}
